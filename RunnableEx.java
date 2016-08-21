4d441fcdbe212957e507ce630d1ed708a97b60b4
Harig22 <ganhari14@gmail.com>
2016-08-21 16:51:07 +0530

    Create RunnableEx.java

---
+++ RunnableEx.java
@@ -0,0 +1,19 @@
+
+public class RunnableEx implements Runnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RunnableEx re=new RunnableEx();
+		Thread t=new Thread(re);
+		Thread t2=new Thread(re);
+		t.start();
+		t2.start();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("I am runnable implementation"+Thread.currentThread());
+	}
+
+}
